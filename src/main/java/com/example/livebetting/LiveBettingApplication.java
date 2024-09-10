ba76ba87da5fdee01cd1d45da70599293d23705f
alperen <alperenkarail93@gmail.com>
2024-09-11 00:34:35 +0300

    livebetting initial commit.

---
+++ src/main/java/com/example/livebetting/LiveBettingApplication.java
@@ -0,0 +1,13 @@
+package com.example.livebetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveBettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveBettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/livebetting/LiveBettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livebetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveBettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
