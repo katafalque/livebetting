f96cd535934f72d8fab8fb84a2b8f710feec8546
alperen <alperenkarail93@gmail.com>
2024-09-15 01:33:47 +0300

    feat(add-markets-to-events): scheduled event to add markets to matches every second.

--- src/main/java/com/example/livebetting/LiveBettingApplication.java
+++ src/main/java/com/example/livebetting/LiveBettingApplication.java
@@ -2,12 +2,14 @@ package com.example.livebetting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LiveBettingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiveBettingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LiveBettingApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/livebetting/service/EventService.java
+++ src/main/java/com/example/livebetting/service/EventService.java
@@ -4,4 +4,6 @@ import com.example.livebetting.data.entity.Event;
 
 public interface EventService {
     void addEvent(Event event);
+
+    void addMarketsToEvents();
 }
