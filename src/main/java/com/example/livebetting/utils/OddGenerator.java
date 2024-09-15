9ea1cc8574f1f7e252a9cd436b0e5cd2958e3348
alperen <alperenkarail93@gmail.com>
2024-09-16 00:08:06 +0300

    fix(odd-generator): refactor off generator to prevent sure bet.

--- src/main/java/com/example/livebetting/utils/OddGenerator.java
+++ src/main/java/com/example/livebetting/utils/OddGenerator.java
@@ -2,6 +2,9 @@ package com.example.livebetting.utils;
 
 import com.example.livebetting.data.model.dto.GeneratedOddsDto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class OddGenerator {
@@ -12,11 +15,15 @@ public class OddGenerator {
     }
 
     public static GeneratedOddsDto generateOdds() {
-        // dummy odd generator to replace later
-        GeneratedOddsDto odds = GeneratedOddsDto.builder().build();
-        odds.setAwayTeamOdd(rng.nextDouble());
-        odds.setHomeTeamOdd(rng.nextDouble());
-        odds.setDrawOdd(rng.nextDouble());
-        return odds;
+        List<Double> randomOdds = new ArrayList<>();
+        randomOdds.add(rng.nextDouble(1, 3));
+        randomOdds.add(rng.nextDouble(1, 5));
+        randomOdds.add(rng.nextDouble(1, 9));
+        Collections.shuffle(randomOdds);
+        return GeneratedOddsDto.builder()
+                .homeTeamOdd(randomOdds.get(0))
+                .drawOdd(randomOdds.get(1))
+                .awayTeamOdd(randomOdds.get(2))
+                .build();
     }
 }
---
+++ src/test/java/com/example/livebetting/utils/OddGeneratorTest.java
@@ -0,0 +1,27 @@
+package com.example.livebetting.utils;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class OddGeneratorTes {
+    @Test
+    void should_generate_random_odds() {
+        /* Act */
+        var result = OddGenerator.generateOdds();
+
+        /* Assert */
+        assertThat(result).isNotNull();
+        // prevent sure bet
+        assertTrue(result.getHomeTeamOdd() < 3 || result.getDrawOdd() < 3 || result.getAwayTeamOdd() < 3);
+    }
+}
