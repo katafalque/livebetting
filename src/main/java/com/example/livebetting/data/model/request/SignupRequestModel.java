639b850f70ba2529a0dcccb7f8b8af4d5dac67a8
alperen <alperenkarail93@gmail.com>
2024-09-15 22:38:07 +0300

    fix: add json properties to signup request model.

--- src/main/java/com/example/livebetting/data/model/request/SignupRequestModel.java
+++ src/main/java/com/example/livebetting/data/model/request/SignupRequestModel.java
@@ -1,5 +1,6 @@
 package com.example.livebetting.data.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,12 @@ import lombok.Setter;
 @Setter
 @Builder
 public class SignupRequestModel {
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
+    @JsonProperty("username")
     private String userName;
+    @JsonProperty("password")
     private String password;
 }
